9098c775d29b6abb5e739a5d90beaef9a70daf8b
My <uk472363@yanex.ru>
2024-04-01 10:24:47 +0300

    Contacts

--- ContactList/Controllers/PersonsListViewController.swift
+++ ContactList/Controllers/PersonsListViewController.swift
@@ -35,32 +35,36 @@ extension PersonsListViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        
+        
         switch indexPath.row{
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "PersonsPhoneCell", for: indexPath)
             cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-            let people = personList[indexPath.row]
             
+            let people = personList[indexPath.section]
             var content = cell.defaultContentConfiguration()
+            
             content.text = people.phone
             content.image = UIImage(systemName: "phone.fill")
             
             cell.contentConfiguration = content
             return cell
+            
         default:
             let cellTwo = tableView.dequeueReusableCell(withIdentifier: "PersonsEmailCell", for: indexPath)
             cellTwo.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-            let people = personList[indexPath.row]
             
+            let people = personList[indexPath.section]
             var content = cellTwo.defaultContentConfiguration()
+            
             content.text = people.email
             content.image = UIImage(systemName: "mail.fill")
             
             cellTwo.contentConfiguration = content
             return cellTwo
+            
+            
         }
-        
     }
 }
-    
-   
